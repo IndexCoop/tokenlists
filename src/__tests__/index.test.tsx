3339a108cf7fdd9e06d584ba0d5fd5d1b246c84e
teddy <chuangtheodore@gmail.com>
2021-12-21 11:13:31 -0800

    Add IndexCoopMaticTokens to test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,8 +1,9 @@
-import {MainnetTokens, MaticTokens} from '../index';
+import {MainnetTokens, MaticTokens, IndexCoopMaticTokens} from '../index';
 
 describe('arent empty', () => {
   test('should add up positive numbers', async () => {
     expect(MainnetTokens.length).toBeGreaterThan(0);
     expect(MaticTokens.length).toBeGreaterThan(0);
+    expect(IndexCoopMaticTokens.length).toBeGreaterThan(0);
   });
 });
