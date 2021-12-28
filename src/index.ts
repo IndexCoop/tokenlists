3661a3307fbe25ad81e9f9f81423ec4e51db2c9e
Zachary Parrish <zachary@parrish.io>
2021-12-28 11:13:41 -0500

    feat: now exports mainnet index coop tokens

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,10 @@
  * ```
  */
 
-import MAINNET_TOKENS from './lists/mainnet';
+import MAINNET_TOKENS, {INDEX_COOP_MAINNET_TOKENS} from './lists/mainnet';
 import MATIC_TOKENS, {INDEX_COOP_MATIC_TOKENS} from './lists/matic';
 
 export const MainnetTokens = MAINNET_TOKENS;
+export const IndexCoopMainnetTokens = INDEX_COOP_MAINNET_TOKENS;
 export const MaticTokens = MATIC_TOKENS;
 export const IndexCoopMaticTokens = INDEX_COOP_MATIC_TOKENS;
