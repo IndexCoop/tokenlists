fd88dc725a9ea92e5a48fa268f4d79665dee9daf
tytarman <83655654+tytarman@users.noreply.github.com>
2022-01-30 23:43:20 -0600

    feat(new tokenlists): created tokenlists for Arbitrum and Optimism
    Fixes issues #9 and #10

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,11 @@
  * ```
  */
 
+import ARBITRUM_TOKENS, {INDEX_COOP_ARBITRUM_TOKENS} from './lists/arbitrum';
 import AVALANCHE_TOKENS, {INDEX_COOP_AVALANCHE_TOKENS} from './lists/avalanche';
 import MAINNET_TOKENS, {INDEX_COOP_MAINNET_TOKENS} from './lists/mainnet';
 import MATIC_TOKENS, {INDEX_COOP_MATIC_TOKENS} from './lists/matic';
+import OPTIMISM_TOKENS, {INDEX_COOP_OPTIMISM_TOKENS} from './lists/optimism';
 
 export interface TokenData {
   chainId: number;
@@ -26,9 +28,13 @@ export interface TokenData {
   logoURI: string;
 }
 
+export const ArbitrumTokens = ARBITRUM_TOKENS;
+export const IndexCoopArbitrumTokens = INDEX_COOP_ARBITRUM_TOKENS;
 export const AvalancheTokens = AVALANCHE_TOKENS;
 export const IndexCoopAvalancheTokens = INDEX_COOP_AVALANCHE_TOKENS;
 export const MainnetTokens = MAINNET_TOKENS;
 export const IndexCoopMainnetTokens = INDEX_COOP_MAINNET_TOKENS;
 export const MaticTokens = MATIC_TOKENS;
 export const IndexCoopMaticTokens = INDEX_COOP_MATIC_TOKENS;
+export const OptimismTokens = OPTIMISM_TOKENS;
+export const IndexCoopOptimismTokens = INDEX_COOP_OPTIMISM_TOKENS;
