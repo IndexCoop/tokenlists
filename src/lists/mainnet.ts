dfcbd3e972490e9ec389babcadfad77403102f34
JD <jann.driessen@gmx.de>
2023-07-28 17:19:56 +0200

    feat: add sfrxeth

--- src/lists/mainnet.ts
+++ src/lists/mainnet.ts
@@ -47489,6 +47489,15 @@ const MAINNET_TOKENS: TokenData[] = [
       'https://assets.coingecko.com/coins/images/21205/thumb/200x200.png?1638551617',
   },
   {
+    address: '0xac3E018457B222d93114458476f3E3416Abbe38F',
+    chainId: 1,
+    name: 'Staked Frax Ether',
+    symbol: 'sfrxETH',
+    decimals: 18,
+    logoURI:
+      'https://assets.coingecko.com/coins/images/28285/large/sfrxETH_icon.png',
+  },
+  {
     chainId: 1,
     address: '0x67725fb3751c257920a88c543ba9b4ba8cfbfa5d',
     name: 'Smoke',
