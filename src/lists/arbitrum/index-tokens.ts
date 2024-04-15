e57f89a19721ba9e2e53fa1a3e8ab730133331b3
JD <jann.driessen@gmx.de>
2024-04-15 09:27:46 +0100

    add eth2x eth3x

--- src/lists/arbitrum/index-tokens.ts
+++ src/lists/arbitrum/index-tokens.ts
@@ -1,3 +1,20 @@
 import {TokenData} from '../../types';
 
-export const INDEX_COOP_ARBITRUM_TOKENS: TokenData[] = [];
+export const INDEX_COOP_ARBITRUM_TOKENS: TokenData[] = [
+  {
+    address: '0x67d2373f0321Cd24a1b58e3c81fC1b6Ef15B205C',
+    chainId: 42161,
+    name: 'Index Coop Ethereum 2x Index',
+    symbol: 'ETH2X',
+    decimals: 18,
+    logoURI: 'https://index-app.vercel.app/assets/eth2x.png',
+  },
+  {
+    address: '0x0bef95Cc308027C9a754D7674DE0844AE1dcD5b1',
+    chainId: 42161,
+    name: 'Index Coop Ethereum 3x Index',
+    symbol: 'ETH3X',
+    decimals: 18,
+    logoURI: 'https://index-app.vercel.app/assets/eth3x.png',
+  },
+];
