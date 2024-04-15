036a1e5e71eebd840cc15f5421474c0a7f4ce949
JD <jann.driessen@gmx.de>
2024-04-15 09:31:14 +0100

    add arbitrum to utils function

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,13 +1,20 @@
 import {getIndexTokenData} from '../utils';
 
 describe('getIndexTokenData', () => {
-  test('returns token data', async () => {
+  test('returns token data for mainnet', async () => {
     const data = getIndexTokenData('MVI');
     expect(data).not.toBeNull();
     expect(data?.symbol).toBe('MVI');
   });
 
-  test('returns null for non mainnet', async () => {
+  test('returns token data for arbitrum', async () => {
+    const data = getIndexTokenData('ETH2X', 42161);
+    expect(data).not.toBeNull();
+    expect(data?.address).toBe('0x67d2373f0321Cd24a1b58e3c81fC1b6Ef15B205C');
+    expect(data?.symbol).toBe('ETH2X');
+  });
+
+  test('returns null for an unsupported chain', async () => {
     const data = getIndexTokenData('MVI', 11155111);
     expect(data).toBeNull();
   });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,20 @@
-import {IndexCoopMainnetTokens, TokenData} from '..';
+import {IndexCoopArbitrumTokens, IndexCoopMainnetTokens, TokenData} from '..';
 
 export function getIndexTokenData(
   symbol: string,
   chainId = 1
 ): TokenData | null {
-  if (chainId !== 1) return null;
-  const tokenData = IndexCoopMainnetTokens.filter(
+  let tokenlist: TokenData[] | null = null;
+  switch (chainId) {
+    case 1:
+      tokenlist = IndexCoopMainnetTokens;
+      break;
+    case 42161:
+      tokenlist = IndexCoopArbitrumTokens;
+      break;
+  }
+  if (tokenlist === null) return null;
+  const tokenData = tokenlist.filter(
     token => token.symbol.toLowerCase() === symbol.toLowerCase()
   );
   return tokenData[0] ?? null;
