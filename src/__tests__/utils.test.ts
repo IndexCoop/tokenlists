5b3d7102cfb6cd11fb56933f0e283ef1059ce1aa
JD <jann.driessen@gmx.de>
2024-07-01 10:36:20 +0200

    feat: add tokenlist utils

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,4 +1,9 @@
-import {getIndexTokenData, getIndexTokenDataByAddress} from '../utils';
+import {
+  getIndexTokenData,
+  getIndexTokenDataByAddress,
+  getTokenData,
+  getTokenDataByAddress,
+} from '../utils';
 
 describe('getIndexTokenData', () => {
   test('returns token data for mainnet', async () => {
@@ -55,6 +60,74 @@ describe('getIndexTokenDataByAddress', () => {
 
   test('returns null for non existent token', async () => {
     const data = getIndexTokenDataByAddress(
+      '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+      1
+    );
+    expect(data).toBeNull();
+  });
+});
+
+describe('getTokenData', () => {
+  test('returns token data for mainnet', async () => {
+    const data = getTokenData('USDC', 1);
+    expect(data).not.toBeNull();
+    expect(data?.address).toEqual('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48');
+    expect(data?.decimals).toEqual(6);
+    expect(data?.symbol).toEqual('USDC');
+  });
+
+  test('returns token data for arbitrum', async () => {
+    const data = getTokenData('USDC', 42161);
+    expect(data).not.toBeNull();
+    expect(data?.address).toEqual('0xaf88d065e77c8cC2239327C5EDb3A432268e5831');
+    expect(data?.decimals).toEqual(6);
+    expect(data?.symbol).toEqual('USDC');
+  });
+
+  test('returns null for an unsupported chain', async () => {
+    const data = getTokenData('USDC', 11155111);
+    expect(data).toBeNull();
+  });
+
+  test('returns null for non existent token', async () => {
+    const data = getTokenData('EURO', 1);
+    expect(data).toBeNull();
+  });
+});
+
+describe('getTokenDataByAddress', () => {
+  test('returns token data for mainnet', async () => {
+    const data = getTokenDataByAddress(
+      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      1
+    );
+    expect(data).not.toBeNull();
+    expect(data?.address).toEqual('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48');
+    expect(data?.decimals).toEqual(6);
+    expect(data?.symbol).toEqual('USDC');
+  });
+
+  test('returns token data for arbitrum', async () => {
+    const data = getTokenDataByAddress(
+      '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+      42161
+    );
+    expect(data).not.toBeNull();
+    expect(data?.address).toEqual('0xaf88d065e77c8cC2239327C5EDb3A432268e5831');
+    expect(data?.decimals).toEqual(6);
+    expect(data?.symbol).toEqual('USDC');
+  });
+
+  test('returns null for an unsupported chain', async () => {
+    const data = getTokenDataByAddress(
+      '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+      11155111
+    );
+    expect(data).toBeNull();
+  });
+
+  test('returns null for non existent token', async () => {
+    const data = getTokenDataByAddress(
       '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
       1
     );
