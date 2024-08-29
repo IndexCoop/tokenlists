62a121052a96bd9e676a7f657cecfe9fcbb466f1
JD <jann.driessen@gmx.de>
2024-08-29 14:41:38 +0200

    feat: add base tokens

---
+++ src/lists/base/base.ts
@@ -0,0 +1,38 @@
+import {TokenData} from '../../types';
+
+export const BASE: TokenData[] = [
+  {
+    name: 'Dai Stablecoin',
+    address: '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb',
+    symbol: 'DAI',
+    decimals: 18,
+    chainId: 8453,
+    logoURI: 'https://assets.coingecko.com/coins/images/9956/large/4943.png',
+  },
+  {
+    name: 'USD Base Coin',
+    address: '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA',
+    symbol: 'USDbC',
+    decimals: 6,
+    chainId: 8453,
+    logoURI:
+      'https://assets.coingecko.com/coins/images/31164/large/baseusdc.jpg',
+  },
+  {
+    name: 'USD Coin',
+    address: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+    symbol: 'USDC',
+    decimals: 6,
+    chainId: 8453,
+    logoURI:
+      'https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png',
+  },
+  {
+    name: 'Wrapped Ether',
+    address: '0x4200000000000000000000000000000000000006',
+    symbol: 'WETH',
+    decimals: 18,
+    chainId: 8453,
+    logoURI: 'https://assets.coingecko.com/coins/images/2518/large/weth.png',
+  },
+];
