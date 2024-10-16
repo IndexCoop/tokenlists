533a50848b0074dfd061e58ef06d46e6919629f2
Daniel <tokdaniel4@gmail.com>
2024-10-16 14:00:54 +0200

    manage crypto with crypto browserify

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  tsconfig: './tsconfig.json',
+  entry: ["./src/index.ts"],
+  tsconfig: "./tsconfig.json",
   splitting: false,
-  format: ['cjs', 'esm'],
+  format: ["cjs", "esm"],
   minify: true,
   clean: true,
   sourcemap: true,
   dts: true,
-  publicDir: './dist',
-  external: ['node:crypto'],
+  publicDir: "./dist",
+  external: ["node:crypto"],
+  esbuildOptions(options) {
+    options.plugins = [
+      {
+        name: "replace-node-crypto",
+        setup(build) {
+          build.onResolve({ filter: /^node:crypto$/ }, (args) => {
+            if (build.initialOptions.platform === "browser") {
+              return { path: require.resolve("crypto-browserify") };
+            } else {
+              return { path: "crypto" };
+            }
+          });
+        },
+      },
+    ];
+  },
 });
