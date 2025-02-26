4f4b53e39c698ded026b0cb17f1f22661a3c86d7
JD <jann.driessen@gmx.de>
2025-02-26 08:52:01 +0100

    add usol2x and usol3x

--- src/tokenlist.json.ts
+++ src/tokenlist.json.ts
@@ -609,6 +609,38 @@ export default {
       tags: ['index', 'product', 'leverage', 'dangerous'],
     },
     {
+      address: '0x0A0Fbd86d2dEB53D7C65fecF8622c2Fa0DCdc9c6',
+      name: 'uSOL/USDC 2x  Leverage Morpho',
+      symbol: 'uSOL2x',
+      decimals: 18,
+      chainId: 8453,
+      extensions: {
+        status: 'Active',
+        coingeckoId: null,
+        leverage: {
+          type: 'Long2x',
+        },
+      },
+      logoURI: 'https://cdn.indexcoop.com/usol2x.svg',
+      tags: ['index', 'product', 'leverage', 'dangerous'],
+    },
+    {
+      address: '0x0A0Fbd86d2dEB53D7C65fecF8622c2Fa0DCdc9c6',
+      name: 'uSOL/USDC 3x Leverage Morpho',
+      symbol: 'uSOL3x',
+      decimals: 18,
+      chainId: 8453,
+      extensions: {
+        status: 'Active',
+        coingeckoId: null,
+        leverage: {
+          type: 'Long3x',
+        },
+      },
+      logoURI: 'https://cdn.indexcoop.com/usol3x.svg',
+      tags: ['index', 'product', 'leverage', 'dangerous'],
+    },
+    {
       address: '0xc8DF827157AdAf693FCb0c6f305610C28De739FD',
       name: 'wstETH Morpho Looped 15x',
       symbol: 'wstETH15x',
